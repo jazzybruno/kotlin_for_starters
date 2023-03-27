705f8bbcf850834cab90c885a0eb8d268e201f65
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-03-28 01:06:18 +0200

    Break and Continue Statemrnts in kotlin

--- src/main/kotlin/Control_Statements.kt
+++ src/main/kotlin/Control_Statements.kt
@@ -51,12 +51,30 @@ fun main(args: Array<String>){
         i--
     }while(i > 0)
 //    Break and continue
-    var i = 0;
     while (i++ < 100) {
         println(i)
         if( i == 3 ){
             break
         }
 
+    }
+
+    outerLoop@ for (i in 1..3) {
+        innerLoop@ for (j in 1..3) {
+            println("i = $i and j = $j")
+            if (i == 2){
+                break@outerLoop
+            }
+            break@innerLoop
+        }
+    }
+
+    outerLoop@ for (i in 1..3) {
+        innerLoop@ for (j in 1..3) {
+            if (i == 2){
+                continue@outerLoop
+            }
+            println("i = $i and j = $j")
+        }
     }
 }
