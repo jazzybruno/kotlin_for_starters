6f3cca5e62e43e77f8f9b424d91252a5f3a2ba34
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-03-28 00:44:42 +0200

    If and else statements in kotlin

---
+++ src/main/kotlin/Control_Statements.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>){
+    // control statements in kotlin
+//    if ... else expressions
+    var age : Int = 21;
+    if(age < 18){
+        println("Minor")
+    }else{
+        println("Adult")
+    }
+
+    if(age < 10) println("She or he is a child") else if(age < 18) println("She or he is a teenager") else println("She or he is and adult")
+//    when expressions
+//    for loop
+//    while loop
+//    Break and continue
+}
