31a8ec108703ed025e398dbf6a0007c2a09621ae
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-03-28 00:27:25 +0200

    Functions in kotlin

---
+++ src/main/kotlin/Functions.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>){
+    var num1 = 10;
+    var num2 = 20;
+    var result : Int = calculate(num1 , num2 , ::add);
+    println(result);
+}
+
+fun printSum(num1:Int , num2:Int){
+    println(num1 + num2)
+}
+
+fun add(num1:Int , num2:Int):Int{
+    return num1 + num2;
+}
+
+// High order Functions ( HOF)
+fun calculate(a:Int , b:Int , operation:(Int , Int) -> Int) : Int{
+   return operation(a , b);
+}
