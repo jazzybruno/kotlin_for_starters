200e9bc151712c444c95642576f92f1af0d4fcf3
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-03-28 04:13:54 +0200

    Sets and collection in Kotlin

---
+++ src/main/kotlin/Sets.kt
@@ -0,0 +1,29 @@
+fun main(){
+    val theSet = setOf<String>("one", "two", "three", "four");
+    var themutableSet = mutableSetOf<String>("one", "two", "three", "four")
+
+    // iterating through
+    // the iterator object
+    var itr = theSet.asIterable().iterator()
+    while (itr.hasNext()){
+        println(itr.next());
+    }
+
+    // for loop
+    println("Printing the values of the set using a for loop");
+    for (i in theSet.indices){
+        println(theSet.elementAt(i))
+    }
+
+    println("Printing the values of the set using a foreach");
+    theSet.forEach{
+        println("The element is $it");
+    }
+
+    // the size
+    println("Printing the size of the set");
+    println("I am printing the size which is:" + theSet.size)
+
+    // in the case of the mutable sets we can also use the add and remove to add anf remove items
+
+}
